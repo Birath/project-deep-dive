1088128397771a3f855fed944d498c349abde127
GrillexN <alfons.jansson98@gmail.com>
2020-10-18 18:33:21 +0200

    Kaotisk spawning av båtar på ytan

--- scenes/EnemyManager.gd
+++ scenes/EnemyManager.gd
@@ -2,14 +2,11 @@ extends Spatial
 
 var enemy = preload("res://entities/enemies/enemy_sub.tscn")
 var mine = preload("res://entities/enemies/mine.tscn")
-#var boat = preload("res://entities/enemies/enemy_sub.tscn")
+var boat = preload("res://assets/enemies/attack_boat.tscn") #Flytta attack_boat till entities?
 var isSpawned = 0
 var rng = RandomNumberGenerator.new()
-#var p = get_parent().get_node("Player")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,24 +16,24 @@ func _ready():
 	var length = node.size()
 	for n in range(length):
 		spawn_mine(n,5)
-		return
+		spawn_boat(n)
 	
 
 	
 func _process(_delta):
-	var player_points = get_node("../goal_manager").complete_sources #Scannable nodes från scannable source?
+	var player_points = get_node("../goal_manager").complete_sources 
 	if player_points >= 1 && isSpawned == 0:
 		spawn_e()
 		isSpawned = 1
 
 func spawn_e():
 	var s = enemy.instance()
-	add_child(s)
 	var player = get_node("../Player")
 	var posx = player.global_transform.origin.x
 	var posy = player.global_transform.origin.y
 	var posz = player.global_transform.origin.z
 	s.translate(Vector3(posx+20, posy+10, posz))
+	add_child(s)
 	
 	
 const grid_space = 20
@@ -63,3 +60,34 @@ func spawn_mine(var i, var grid_size):
 			posz+ (cor.y-grid_size/2.0) *grid_space + rng.randf_range(-5, 5)
 		))
 		add_child(s)
+		
+func spawn_boat(var i):
+	
+	var node = get_node("../goal_manager").positions
+	var posx = node[i].x + rng.randf_range(35, 120)
+	var posy = 155 #?
+	var posz = node[i].z + rng.randf_range(35, 120)
+	for index in range (3): #Kaotisk spawning av båtar
+		var s = boat.instance()
+
+		s.translate(Vector3(posx+(index*rng.randf_range(70, 220)), posy, posz+(index*rng.randf_range(70, 220))))
+		add_child(s)
+		s.transform.basis = transform.basis.rotated(Vector3(0, 1, 0), PI/(index+1))
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
